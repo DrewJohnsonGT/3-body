30821bbba4f3a535ebe0c20dac24f3eddbcd086e
Drew Johnson <drewjohnsongt@gmail.com>
2024-07-19 23:01:48 -0400

    Bootstrapping config and app setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    server: {
+      open: true,
+    },
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    },
+  };
+});
