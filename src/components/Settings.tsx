dbebabbf408b8315d623b37aee5d29b2f858dd0d
Drew Johnson <drewjohnsongt@gmail.com>
2024-08-03 20:56:28 -0400

    Styling for new body color preview

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -69,166 +69,166 @@ export const Settings = () => {
   } = useAppContext();
 
   return (
-      <IonContent>
-        <IonList lines="full">
-          <IonItem>
-            <IonToggle
-              enableOnOffLabels={true}
-              labelPlacement="start"
-              checked={tapToCreate}
-              onIonChange={() => {
-                dispatch({
-                  type: ActionType.ToggleTapToCreate,
-                });
-              }}>
-              <IonLabel>
-                <IonText>Tap to create body</IonText>
-                <br />
-                <IonNote color="medium" className="ion-text-wrap">
-                  Tapping on the screen will create a new body at that location
-                </IonNote>
-              </IonLabel>
-            </IonToggle>
-          </IonItem>
-          <IonItem>
-            <IonToggle
-              enableOnOffLabels={true}
-              labelPlacement="start"
-              checked={showTrails}
-              onIonChange={() => {
-                dispatch({
-                  payload: !showTrails,
-                  type: ActionType.SetShowTrails,
-                });
-              }}>
-              <IonLabel>
-                <IonText>Show trails</IonText>
-                <br />
-                <IonNote color="medium" className="ion-text-wrap">
-                  Bodies will leave a trail behind them
-                </IonNote>
-              </IonLabel>
-            </IonToggle>
-          </IonItem>
-          {showTrails && (
-            <RangeSettingsItem
-              label="Trail length"
-              value={trailLength}
-              onIonChange={(value) => {
-                dispatch({
-                  payload: value,
-                  type: ActionType.SetTrailLength,
-                });
-              }}
-              rangeProps={{
-                max: 500,
-                min: 1,
-              }}
-            />
-          )}
+    <IonContent>
+      <IonList lines="full">
+        <IonItem>
+          <IonToggle
+            enableOnOffLabels={true}
+            labelPlacement="start"
+            checked={tapToCreate}
+            onIonChange={() => {
+              dispatch({
+                type: ActionType.ToggleTapToCreate,
+              });
+            }}>
+            <IonLabel>
+              <IonText>Tap to create body</IonText>
+              <br />
+              <IonNote color="medium" className="ion-text-wrap">
+                Tapping on the screen will create a new body at that location
+              </IonNote>
+            </IonLabel>
+          </IonToggle>
+        </IonItem>
+        <IonItem>
+          <IonToggle
+            enableOnOffLabels={true}
+            labelPlacement="start"
+            checked={showTrails}
+            onIonChange={() => {
+              dispatch({
+                payload: !showTrails,
+                type: ActionType.SetShowTrails,
+              });
+            }}>
+            <IonLabel>
+              <IonText>Show trails</IonText>
+              <br />
+              <IonNote color="medium" className="ion-text-wrap">
+                Bodies will leave a trail behind them
+              </IonNote>
+            </IonLabel>
+          </IonToggle>
+        </IonItem>
+        {showTrails && (
           <RangeSettingsItem
-            label="Gravity multiplier"
-            value={gravityMultiplier}
+            label="Trail length"
+            value={trailLength}
             onIonChange={(value) => {
               dispatch({
                 payload: value,
-                type: ActionType.SetGravityMultiplier,
+                type: ActionType.SetTrailLength,
               });
             }}
             rangeProps={{
-              max: 10,
-              min: 0,
+              max: 500,
+              min: 1,
             }}
           />
+        )}
+        <RangeSettingsItem
+          label="Gravity multiplier"
+          value={gravityMultiplier}
+          onIonChange={(value) => {
+            dispatch({
+              payload: value,
+              type: ActionType.SetGravityMultiplier,
+            });
+          }}
+          rangeProps={{
+            max: 10,
+            min: 0,
+          }}
+        />
+        <IonItem>
+          <IonGrid>
+            <IonRow class="ion-margin-bottom">
+              <IonLabel>
+                <IonText>New Body</IonText>
+                <br />
+                <IonNote color="medium" className="ion-text-wrap">
+                  New bodies will be created with random or custom properties -
+                  mass and color
+                </IonNote>
+              </IonLabel>
+            </IonRow>
+            <IonRow>
+              <IonSegment
+                value={newBodyType}
+                onIonChange={({ detail }) => {
+                  detail.value &&
+                    dispatch({
+                      payload: detail.value as NewBodyType,
+                      type: ActionType.SetNewBodyType,
+                    });
+                }}>
+                <IonSegmentButton value="random">
+                  <IonLabel>Random</IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="custom">
+                  <IonLabel>Custom</IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+            </IonRow>
+          </IonGrid>
+        </IonItem>
+        {newBodyType === 'custom' && (
+          <RangeSettingsItem
+            label="Mass"
+            value={newBodyMass}
+            onIonChange={(value) => {
+              dispatch({
+                payload: value,
+                type: ActionType.SetNewBodyMass,
+              });
+            }}
+            rangeProps={{
+              max: 100,
+              min: 1,
+            }}
+          />
+        )}
+        {newBodyType === 'custom' && (
           <IonItem>
             <IonGrid>
-              <IonRow class="ion-margin-bottom">
+              <IonRow>
                 <IonLabel>
-                  <IonText>New Body</IonText>
+                  <IonText>Color</IonText>
                   <br />
                   <IonNote color="medium" className="ion-text-wrap">
-                    New bodies will be created with random or custom properties
-                    - mass and color
+                    Select the color of newly created bodies
                   </IonNote>
                 </IonLabel>
               </IonRow>
-              <IonRow>
-                <IonSegment
-                  value={newBodyType}
-                  onIonChange={({ detail }) => {
-                    detail.value &&
-                      dispatch({
-                        payload: detail.value as NewBodyType,
-                        type: ActionType.SetNewBodyType,
-                      });
-                  }}>
-                  <IonSegmentButton value="random">
-                    <IonLabel>Random</IonLabel>
-                  </IonSegmentButton>
-                  <IonSegmentButton value="custom">
-                    <IonLabel>Custom</IonLabel>
-                  </IonSegmentButton>
-                </IonSegment>
+              <IonRow class="ion-padding ion-justify-content-center ion-align-items-center">
+                <HexColorPicker
+                  color={newBodyColor}
+                  onChange={(color) => {
+                    dispatch({
+                      payload: color,
+                      type: ActionType.SetNewBodyColor,
+                    });
+                  }}
+                />
+              </IonRow>
+              <IonRow className="ion-padding ion-justify-content-center ion-align-items-center">
+                <IonLabel>
+                  <IonText
+                    style={{
+                      backgroundColor: getBackgroundColor(newBodyColor),
+                      border: `2px solid ${newBodyColor}`,
+                      borderRadius: '0.5rem',
+                      color: newBodyColor,
+                      padding: '0.5rem',
+                    }}>
+                    {newBodyColor}
+                  </IonText>
+                </IonLabel>
               </IonRow>
             </IonGrid>
           </IonItem>
-          {newBodyType === 'custom' && (
-            <RangeSettingsItem
-              label="Mass"
-              value={newBodyMass}
-              onIonChange={(value) => {
-                dispatch({
-                  payload: value,
-                  type: ActionType.SetNewBodyMass,
-                });
-              }}
-              rangeProps={{
-                max: 100,
-                min: 1,
-              }}
-            />
-          )}
-          {newBodyType === 'custom' && (
-            <IonItem>
-              <IonGrid>
-                <IonRow>
-                  <IonLabel>
-                    <IonText>Color</IonText>
-                    <br />
-                    <IonNote color="medium" className="ion-text-wrap">
-                      Select the color of newly created bodies
-                    </IonNote>
-                  </IonLabel>
-                </IonRow>
-                <IonRow class="ion-padding">
-                  <HexColorPicker
-                    color={newBodyColor}
-                    onChange={(color) => {
-                      dispatch({
-                        payload: color,
-                        type: ActionType.SetNewBodyColor,
-                      });
-                    }}
-                  />
-                </IonRow>
-                <IonRow>
-                  <IonCol size="12">
-                    <IonLabel>
-                      <IonText
-                        style={{
-                          backgroundColor: getBackgroundColor(newBodyColor),
-                          color: newBodyColor,
-                          padding: '0.5rem',
-                        }}>
-                        {newBodyColor}
-                      </IonText>
-                    </IonLabel>
-                  </IonCol>
-                </IonRow>
-              </IonGrid>
-            </IonItem>
-          )}
-        </IonList>
-      </IonContent>
+        )}
+      </IonList>
+    </IonContent>
   );
 };
